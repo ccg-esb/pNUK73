f336c2d4aaf7fbf0312fafba74d2e3860e3d4b42
ccg-esb <r.penamiller@gmail.com>
2014-07-31 23:17:04 -0500

    Update ODfit.c

--- src/ODfit.c
+++ src/ODfit.c
@@ -1,11 +1,25 @@
-//  ODfit model used by package deSolve in pNUK73
+// ODfit model used by package pNUK73
 //
-//  Created by RPM on 28/5/14.
+// Created by RPM on 28/5/14.
+// Copyright 2014. All rights reserved.
 //
 // Implements deterministic batch model with one bacterial type and a single-limiting resource to be used with R 
 // To compile from R: system("R CMD SHLIB ODfit.c")
 // To load from R (in Unix): dyn.load("ODfit.so")
 // To call from R use deSolve package and see details in help on using compiled code.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <R.h> 
 
